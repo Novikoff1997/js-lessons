235873ca2e9bf7fd98be7d2615f25f65c541acb6
Novikoff1997 <andreytoys@yandex.ru>
2024-12-28 21:17:59 +0300

    Домашнее задание к первому уроку

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "";
+const screens = "";
+const screenPrice = "";
+const rollback = "";
+const fullPrice = "";
+const adaptive = "";
+
+alert("Hello, world!");
+
+console.log("Привет, мир!");
