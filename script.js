caed404f991eb77a9d06083b5b3d5a443f5ed031
Novikoff1997 <andreytoys@yandex.ru>
2025-01-02 11:23:46 +0300

    Домашнее задание к уроку 3

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-let title = "js-lessons";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 3;
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let rollback = 10;
 let fullPrice = 100000000;
-let adaptive = true;
+let adaptive = !!prompt("Нужен ли адаптив на сайте?");
 
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = parseInt(+prompt("Сколько это будет стоить?"));
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = parseInt(+prompt("Сколько это будет стоить?"));
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
+console.log("Итоговая стоимость работы: " + fullPrice + "₽");
+console.log("Итоговая стоимость работы за вычетом отката посреднику: " + servicePercentPrice + "₽");
+
+if (fullPrice > 30000) {
+  fullPrice = fullPrice - (fullPrice / 100) * 10;
+  console.log("Вам предоставлена скидка в 10%. Итоговая сумма: " + fullPrice);
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+  fullPrice = fullPrice - (fullPrice / 100) * 5;
+  console.log("Вам доступна скидка в 5%. Итоговая самму: " + fullPrice);
+} else if (fullPrice < 15000 && fullPrice > 0) {
+  console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+  console.log("Что то пошло не так!");
+}
 alert("Hello, world!");
 
 console.log("Привет, мир!");
